951cab6c44da01bf371303ad16b2f4c7f0d1a3fa
Durvesh725 <durveshjagtap45@gmail.com>
2024-08-17 19:17:28 +0530

    backend_url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,11 +1,12 @@
 import axios from 'axios'
 import { ACCESS_TOKEN } from "./constants"
+const apiUrl = "/choreo-apis/organized--notes-app/backend/v1"
 
 // Axios instance with a base URL from the environment variable
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL   
+    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl
     // import env file to use backend url
-})
+});
 
 // Axios request interceptor to add the Authorization header to each request
 api.interceptors.request.use(
